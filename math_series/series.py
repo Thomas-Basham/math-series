4640b6a426d9cbca679a2eadf21e3eb2e0f3c8c0
Thomas Basham <Bashamtg@gmail.com>
2022-04-24 12:37:05 -0700

    sum series

--- math_series/series.py
+++ math_series/series.py
@@ -1,10 +1,10 @@
 # Function for nth Fibonacci number
-def fibonacci(n):
+def fibonacci(n, a=2, b=1):
     # Declare placeholder
     # for position 2
-    if n == 2:
+    if n == a:
         return 1
-    elif n == 0 or n == 1:
+    elif n == 0 or n == b:
         return 0
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
@@ -14,9 +14,9 @@ print(fibonacci(2))
 
 
 # Function for nth Lucas number
-def lucas(n):
-    a = 2
-    b = 1
+def lucas(n, a=2, b=1):
+    # a = 2
+    # b = 1
     # declare placeholders
     # for positions 0 and 1
     if n == 0 or n == 1:
@@ -28,4 +28,13 @@ def lucas(n):
     return b
 
 
-print(lucas(1))
+print(lucas(1,))
+
+
+def sum_series(n, a=None, b=None):
+    if a or b:
+        return lucas(n, a, b)
+    else:
+        return fibonacci(n)
+
+
